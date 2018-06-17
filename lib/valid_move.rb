04e69bb1b0397f0bca12b31a66c55225fec519a1
Trevor Jameson <tjameson15@gmail.com>
2018-06-17 17:42:42 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,8 @@ def valid_move?(board, index)
   index -= 1
   if (index.between?(0, 8 )) && !(position_taken?(board, index))
     return true
+  elsif position_taken?(board, index)
+    return false
   else
     return false
   end
